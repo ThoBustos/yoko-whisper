11c1efdc5f515562bbe6a9bca177e90469b02655
Thomas Bustos <98232880+ThoBustos@users.noreply.github.com>
2026-08-31 13:10:46 +0200

    fix: use Dependabot-compatible Swift requirement

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "YokoWhisper",
     platforms: [.macOS(.v14)],
     products: [.executable(name: "YokoWhisper", targets: ["YokoWhisper"])],
-    dependencies: [.package(url: "https://github.com/argmaxinc/argmax-oss-swift.git", exact: "1.1.0")],
+    dependencies: [.package(url: "https://github.com/argmaxinc/argmax-oss-swift.git", from: "1.1.0")],
     targets: [
         .executableTarget(name: "YokoWhisper", dependencies: [.product(name: "WhisperKit", package: "argmax-oss-swift")], path: "YokoWhisper", exclude: ["YokoWhisper.entitlements"], resources: [.process("PrivacyInfo.xcprivacy")]),
         .testTarget(name: "YokoWhisperTests", dependencies: ["YokoWhisper"], path: "YokoWhisperTests")
